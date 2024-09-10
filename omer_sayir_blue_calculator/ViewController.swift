7ad01ba1f843b9b555f6aed559c44b70daef2bd2
omercsx <omercagris@gmail.com>
2024-09-10 06:21:56 -0400

    Initial Commit

---
+++ omer_sayir_blue_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  omer_sayir_blue_calculator
+//
+//  Created by Omer Cagri Sayir on 10.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
