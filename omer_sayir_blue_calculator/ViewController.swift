07b5a8b3e334a2dfa0c7ce2087c1bd17d3decee2
omercsx <omercagris@gmail.com>
2024-09-10 10:38:34 -0400

    Reading button

--- omer_sayir_blue_calculator/ViewController.swift
+++ omer_sayir_blue_calculator/ViewController.swift
@@ -12,8 +12,32 @@ class ViewController: UIViewController {
 
     @IBOutlet var numberDisplay: UITextField!
 
+    // MARK: - Functions
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Actions
+
+    @IBAction func buttonTouched(_ sender: UIButton) {
+        var currentValue = numberDisplay.text!
+        if let buttonText = sender.titleLabel?.text {
+            switch buttonText {
+            case "1", "2", "3", "4", "5", "6", "7", "8", "9", "0":
+                if currentValue == "0" {
+                    currentValue = buttonText
+                } else {
+                    currentValue = currentValue + buttonText
+                }
+            case "C":
+                currentValue = "0"
+            default:
+                print("something else")
+            }
+
+            numberDisplay.text = currentValue
+        }
     }
 }
